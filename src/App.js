e89be7415c4ba191a3eaf491b02537eb76745933
ElenaTkacheva <79642915+ElenaTkacheva@users.noreply.github.com>
2022-02-01 10:22:06 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,152 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { data } from "./data";
+import "./App.css";
 
 function App() {
+
+  const [sights, setSights] = useState(data);
+  const [showMore, setShowMore] = useState(false);
+
+  const removeItem = (id) => {
+    let newSights = sights.filter((element) => element.id !== id);
+    setSights(newSights);
+  };
+
+  // const setShowMore = (id) => {
+  //   const newSights = [];
+  //   sights.forEach((sight) => {
+  //     if (sight.id === id) {
+  //       const changedSight = { ...sight, showMore: !sight.showMore };
+  //       newSights.push(changedSight);
+  //     } else {
+  //       newSights.push(sight);
+  //     }
+  //   });
+  //   setSights(newSights);
+  // };
+
+  const [t, setT] = useState(0);
+  const [datas, setDatas] = useState(data);
+  let newArr = [...datas];
+
+  const [count, setCount] = useState([0, 0, 0, 0, 0, 0]);
+
+  const nextSight = (e, index) => {
+    setDatas(newArr);
+
+    const slide = document.querySelectorAll("#slide");
+    newArr[index] = sights[index];
+
+    setT((t) => {
+      t++;
+      if (t > 2) {
+        t = 0;
+      }
+      return t;
+    });
+    slide[index].setAttribute("src", newArr[index].image[t]);
+    return slide;
+  };
+
+  const previousSight = (e, index) => {
+    const slide = document.querySelectorAll("#slide");
+    newArr[index] = sights[index];
+    setDatas(newArr);
+    setT((t) => {
+      t--;
+      if (t < 0) {
+        t = 2;
+      }
+      return t;
+    });
+
+    slide[index].setAttribute("src", newArr[index].image[t]);
+    return slide;
+  };
+
+  const rateUpdate = (e, index) => {
+    const rateHolder = document.querySelectorAll('#rateHolder');
+    setCount((count => {
+      count[index]++;
+      return count;
+    }))
+    rateHolder[index].innerHTML = (sights[index].rating) + count[index];
+    return rateHolder;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <h1>Top {sights.length} sights in Montreal</h1>
+      </div>
+      {sights.map((element) => {
+        const { id, name, image, description, rating } = element;
+        const index = sights.indexOf(element);
+
+        return (
+          <div className="container" key={id}>
+            <div>
+              <h2>
+                {id}. {name}
+              </h2>
+            </div>
+            <div>
+              <img id="slide" className="photo" src={image[0]} alt="sight" />
+            </div>
+            <div className="buttons">
+              <button
+                className="btn"
+                onClick={(e) => previousSight(e, index)}
+                id={id}
+              >
+                Previous
+              </button>
+              <button
+                className="btn"
+                onClick={(e) => nextSight(e, index)}
+                id={id}
+              >
+                Next
+              </button>
+            </div>
+            <div className="container">
+              <p>
+                {showMore ? description : description.substring(0, 240) + "..."}
+                <button
+                  className="show"
+                  onClick={() => setShowMore(!showMore)}
+                >
+                  {showMore ? "Show less" : "Show more"}
+                </button>
+              </p>
+            </div>
+            <div className="container rate">
+              <h3 className="numberRate" id="rateHolder" value="">
+                {rating}
+              </h3>
+              <button
+                className="btnRate"
+                id={id}
+                onClick={(e) => rateUpdate(e, index)}
+              >
+                {" "}
+                ⭐{" "}
+              </button>
+            </div>
+            <div className="container">
+              <button className="btnRemove" onClick={() => removeItem(id)}>
+                Remove
+              </button>
+            </div>
+          </div>
+        );
+      })}
+      <div className="container">
+        <button className="btnDeleteAll" onClick={() => setSights([])}>
+          {" "}
+          Delete all{" "}
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,80 @@
+export const data = [
+  {
+    id: 1,
+    name: "Notre-Dame Basilica of Montreal",
+    image: [
+      "https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcRPIKfrIzckwGlRaJJ52ahm4Cx2iUXRKc4UF1cQAf8SOEn3MyVhAxtGIk--48wk9S3JzsD8QYdCCdV8oEilGIc1ZQ",
+      "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcQdBWQZ4P7YAjFWsZSVe_9KQ3gX-0by9PF2NDP3NGahmAF91hCzLowIdjEsOxa12rOMr8wWTG4Rt6ISWFQN843_3Q",
+      "https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcT8L2VuKDNaW13udWmGUo37zDClY27VAt2AJ3MdO9BM4ELIsyPPiRMtYh2vCNEk8j9G8Dpg1tvkbI_aPVUbA6rfpA",
+      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQajRI6G77bzXYrJnotlJXK5clS3fYUphiuAX8jg_QgXKK3btYazv1P1J7vdO1ZAX7gLAT3fi6MXw5efP6PoX-yEA",
+    ],
+    description:
+      "Notre-Dame Basilica is a basilica in the historic district of Old Montreal, in Montreal, Quebec, Canada. The church is located at 110 Notre-Dame Street West, at the corner of Saint Sulpice Street. It is located next to the Saint-Sulpice Seminary and faces the Place d'Armes square. The interior of the church is amongst the most dramatic in the world and regarded as a masterpiece of Gothic Revival architecture. The vaults are coloured deep blue and decorated with golden stars, and the rest of the sanctuary is decorated in blues, azures, reds, purples, silver, and gold. It is filled with hundreds of intricate wooden carvings and several religious statues",
+    rating: 0,
+  },
+  {
+    id: 2,
+    name: "The Montreal Museum of Fine Arts",
+    image: [
+      "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTlCUXJIck2AfabeGmdKcuvXm2fefA4scsjs7vU3L6-iv3bgcmz0l-eydqoJev9j0TjHmSn17prQ5mT1-C8RiHddw",
+      "https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcSWxnWzSncV-fVIHUyadOa1yaguqUuYBsB_K_WF4ndfJ_iWsQrAJ8Rr1YFHcDct65dqmCf_Y-bpjghpoqqZoG5vwA",
+      "https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcQpzgOvDE9yB_2LqEprUnGa8r8LVNN8k56uGZxnS7WeA8qi2tdPIst_kQrqq2D2AbE2M6Ht_9jK3vbs7ZIQgcmZCw",
+      "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcR4fjqFOM5Vi2pRtRYtwXYkJFdalW5146ta_OO5M2M_OI6RJvwTlYyoNF3vDRuyRFInITx_A7Q9KizP_Hn1RYL2_g",
+    ],
+    description:
+      "The Montreal Museum of Fine Arts is an art museum in Montreal, Quebec, Canada. It is the largest art museum in Canada by gallery space. The museum is located on the historic Golden Square Mile stretch of Sherbrooke Street. The MMFA is spread across five pavilions, and occupies a total floor area of 53,095 square metres, 13,000 of which are exhibition space. With the 2016 inauguration of the Michal and Renata Hornstein Pavilion for Peace, the museum campus was expected to become the eighteenth largest art museum in North America.",
+    rating: 0,
+  },
+  {
+    id: 3,
+    name: "Mount Royal",
+    image: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB4wADdW_b4tiaW6jahSX0d2cw967jOaYD1HwExQYvZEwTAhGZ7VIqvEN2lBg8Klq07x3_tIRNNE9m37R56dTrlw",
+      "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcRTha_41oTdwcTYb1ssEsWU8puH3c6aYqymIzXW-xuJ5Q-ojg3vPEYHQmWE7csasJMx3kWhod7mqjzHt-OZiUVCbA",
+      "https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcTeZLo4Jn_0uG0WeEi5EQKWYUVKLO-7sQYcyT4MshczB-h6bStmr4rXTG1eBWzmJj0mUyVMMORaBdHH1DM8W1KSug",
+      "https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcQ-502fycuW06Bf-xfBENjyozcTnZ0MmoOS2EyuMNEenNGQ5zpsV82zK3NrshqFTJF1FHye5-35Veoe9kcqh66uIw",
+    ],
+    description:
+      "Mount Royal is a large intrusive rock hill or small mountain in the city of Montreal, immediately west of Downtown Montreal, Quebec, Canada. The best-known hypothesis for the origin of the name Montreal is that the name is taken from Mount Royal. The hill is part of the Monteregian Hills situated between the Laurentians and the Appalachian Mountains. It gave its Latin name, Mons Regius, to the Monteregian chain. The hill consists of three peaks: Colline de la Croix at 233 m, Colline d'Outremont at 211 m, and Westmount Summit at 201 m elevation above mean sea level. ",
+    rating: 0,
+  },
+  {
+    id: 4,
+    name: "Montreal Botanical Garden",
+    image: [
+      "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcSqrPcQ074kLMGyi4hF02PdHpUH2DOljW6JbrXd_ueq3bBy2zTyLnRV0HC1SyNr2OuRhQRAudkuaAcY_RQF6xIB1A",
+      "https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcTsJOpxRNdMnxnqxUxUHwKxHMHqWOLCF0QxV_Aj9IFp5h0_STPpj4HNt1E-YcjmTv4viaqu9fpqzvA_j6ufh6hqVA",
+      "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQJx7Ob3zWNXgar4ckK4TZKhtbfN0mB6aQsOnK5zUVvQnUN7sGo_fROw6GXyfJU3WZyV2vvvnPh9U3oscvFlxtpvg",
+      "https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcTTk1xgVVr4roFESHjZnMUtzzi2AKjvl6nJ4ITPQFLffCaba2QliAlbjPCr-iIYBnkqH_jaYQCHUBxfqx3BIG3CmA",
+    ],
+    description:
+      "The Montreal Botanical Garden is a large botanical garden in Montreal, Quebec, Canada comprising 75 hectares of thematic gardens and greenhouses. It was designated a National Historic Site of Canada in 2008 as it is considered to be one of the most important botanical gardens in the world due to the extent of its collections and facilities.",
+    rating: 0,
+  },
+  {
+    id: 5,
+    name: "Saint Joseph's Oratory of Mount Royal",
+    image: [
+      "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRpxc2qdskgBlL-x1kwEjeLMQuii2wkYilJOKVTw2FrAvlsXvlhBjOFMsXm1Zyc5MxD8PSB7psMxEhpj7_QNo6BCA",
+      "https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcTKRB_mv4pSeCu8L1fRyhwyN4gEcPoXuME308g9OatpHVtayozw4nHfWQGVWaQEhGo6clQ4pf7B8gKtGvqA2ktWNQ",
+      "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcTGyoIZiLGKeYX8MKtHJfENiiA5s9vZmTX0KDYgnXSG3x1l0QPhn_d6r5CIg-i742hDpBp8zGhu6zBxUGI119aLaQ",
+      "https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcSzekFddahCrqeh82PB5ZT6t2xpKseiQOyBch6ihqn1D9ADNQ3nxMx9tghsL1PWljKZsIbfkAzdKSNNzUqYqlqPyQ",
+    ],
+    description:
+      "Saint Joseph's Oratory of Mount Royal is a Roman Catholic minor basilica and national shrine located at 3800 Queen Mary Road on Mount Royal's Westmount Summit in Montreal, Quebec. It is a National Historic Site of Canada and is Canada's largest church, with one of the largest church domes in the world. Founded in 1904 by Saint André Bessette in his patron saint, Saint Joseph's honour, the Oratory was brought alive through the work and ingenuity of numerous architects and thousands of workers in a process spanning six decades. With its monumental scale, Renaissance Revival facade and contrasting Art Deco interior, the Oratory is recognizable not just in Montreal but around the world, attracting more than 2 million visitors and pilgrims to its steps each year.",
+    rating: 0,
+  },
+  {
+    id: 6,
+    name: "La Ronde",
+    image: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL7ZcG2Y3VibPGvBCoZsNoZuwWf5ifpgzJfvT3m4OzoU5KfmAVUzjdZU-zJbJsb20uzwQbBOTIbrK8rTm3bdFryA",
+      "https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcSF_xxu1eufZUI8DORJKWBqSj7AHMdN7p62i5sHnjmX5unbF_U-cx-baqhP6EntTZy2rJQmUgKX7Y83l4hkQM6p7w",
+      "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQZUj8fHsgqH9golKqwOMrbSIZkHFT1nStNQcf4pSrDxnNngNNpaiuXRE2xnMBJl0iM0d1Zsf_50PV8MYz2wLPSiw",
+      "https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcTTvzjbN2iX8X37biEUyWy_hbrzRSGDImvgtAM8ibDN87KQ9cS2FXy4ysHLg5iz50qxuJ3G-naw-rZIKqfqNf1Ejg",
+    ],
+    description:
+      "La Ronde is an amusement park in Montreal, Quebec, Canada, built as the entertainment complex for Expo 67, the 1967 World Fair. Today, it is operated by Six Flags under an emphyteutic lease with the City of Montreal, which expires in 2065. It is the largest amusement park in Quebec and second largest in Canada. It is on 59 hectares located on the Northern tip of Saint Helen's Island. This is a man-made extension to the island in the space where the small Ronde Island had been. The park hosts L'International des Feux Loto-Québec, an international fireworks competition. La Ronde is one of three Six Flags parks not to be officially branded as a Six Flags park, Great Escape in Queensbury, New York, and Frontier City in Oklahoma City, Oklahoma being the other two.",
+    rating: 0,
+  },
+];
